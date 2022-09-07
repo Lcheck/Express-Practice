48af3daaa3f37a4ab8e41c5fcdad32299b3ccd7b
Lcheck <znfhdl789@naver.com>
2022-09-08 02:06:43 +0900

    시퀄라이즈로 Post 테이블 생성하고 서버/DB 동기화

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const {sequelize} = require('./models');
 
 const app = express();
 
@@ -7,6 +8,17 @@ app.get('/',(req,res)=>{
 
 res.send('hello express');
 });
+
+//맨처음에 DB스키마가 만들어져 있어야 오류가 안남, npx sequelize db:create 해주자 config의 설정대로 db가 생성됨
+
+
+sequelize.sync({ force: false })
+   .then(() => {
+      console.log('데이터베이스 연결됨.');
+   }).catch((err) => {
+      console.error(err);
+   });
+//mysql와 sequelize 동기화
 
 
 app.listen(3065,()=>{console.log('서버실행중')});
--- models/index.js
+++ models/index.js
@@ -3,17 +3,15 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+const Post = require('./post');
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+//config의 정보대로 시퀄라이즈와 mysql을 연결함
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+
+db.Post = require('./post')(sequelize);
+//모델 불러오기, 생성된 sequelize객체를 전달함, 모델에서는 해당 객체에 table 생성
 
 module.exports = db;
